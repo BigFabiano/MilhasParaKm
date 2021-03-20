ff29d1eb7d5f31e3b4e581e26adc4d2b3e9ba8ef
FabianoEtec <nbscbs@gmail.com>
2021-03-19 22:33:26 -0300

    Convertido milhas em KM

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Converte Milhas em Km");
+            Console.WriteLine("----------------------");
+
+            double doubleMilhas;
+            double doubleKm;
+            
+            Console.Write(" Quantas milhas você pretende andar hoje? ");
+            doubleMilhas = Convert.ToDouble(Console.ReadLine());
+
+            doubleKm = doubleMilhas * 1.609;
+            Console.WriteLine($" Você percorreu uma distania de {doubleMilhas} milhas, o equivalente a {doubleKm:N2} Km");            
+            
+        }
+    }
+}
